da4d1c119546c9ec6f3bfad0388107f53e8e53ef
Sangeeth <sangeethdar@gmail.com>
2024-02-20 20:28:47 +0530

    Price Logic added

--- lib/screens/product_listing/Product_Listing.dart
+++ lib/screens/product_listing/Product_Listing.dart
@@ -910,10 +910,9 @@ class _HomeScreenState extends State<ProductListing> {
                                   "SimpleProduct"
                                   ? provider.items[index].price.regularPrice.amount.value
                                   .toString()
-                                  : "${provider.items[index].getPriceRange}",
-/*
+                                  :
                               "${provider.items[index].priceRange.minimumPrice.regularPrice.value}"
-                                  " - ${provider.items[index].priceRange.minimumPrice.regularPrice.value}",*/
+                                  " - ${provider.items[index].priceRange.minimumPrice.regularPrice.value}",
                               product: provider.items[index],
                               bgColor: demo_product[0].colors[0],
                               item: provider.items[index],
